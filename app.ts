b0048c7b32e31abc8e02bff2a5bda71025f96156
ilya-bkv <i.yablokov@ccmp.me>
2023-05-15 01:09:42 +0300

    working iteration

--- app.ts
+++ app.ts
@@ -1,53 +1,67 @@
-import tinify from "tinify"
-import * as fs from 'fs'
-import * as path from 'path'
-import * as Table from "cli-table3"
+require('dotenv').config();
 
-tinify.key = process.env['API_KEY'];
-const compressImages = (dir: string, table?: Table) => {
+const tinify = require("tinify");
+const fs = require("fs");
+const path = require("path");
+const Table = require("cli-table3");
+
+tinify.key = process.env.API_KEY;
+
+const compressImages = (dir, table) => {
   fs.readdirSync(dir).forEach((file) => {
     const fullPath = path.join(dir, file);
     if (fs.statSync(fullPath).isDirectory()) {
       compressImages(fullPath, table);
     } else if (
-      path.extname(fullPath).toLowerCase() === ".png" ||
-      path.extname(fullPath).toLowerCase() === ".jpg" ||
-      path.extname(fullPath).toLowerCase() === ".jpeg"
+      [".png", ".jpg", ".jpeg"].includes(path.extname(fullPath).toLowerCase())
     ) {
       const imageDir = path.dirname(fullPath);
       const compressedDir = path.join(imageDir, "compressed");
       if (!fs.existsSync(compressedDir)) {
         fs.mkdirSync(compressedDir);
       }
-      const compressedPath = path.join(compressedDir, path.basename(fullPath));
+      const compressedPath = path.join(
+        compressedDir,
+        path.basename(fullPath)
+      );
       tinify.fromFile(fullPath).toFile(compressedPath, (err) => {
         if (err) {
           console.log(err);
           return;
         }
-        const beforeSize = fs.statSync(dir).size;
+        const beforeSize = fs.statSync(fullPath).size;
         const afterSize = fs.statSync(compressedPath).size;
 
-        const table = new Table({
-          head: ["File ", "Before", "After"],
+        const tableData = [
+          [path.basename(fullPath), `${beforeSize} B`, `${afterSize} B`],
+        ];
+
+        const tableConfig = {
+          head: ["File", "Before", "After"],
           style: {
-            head: ['green'],
-            border: ['blue'],
-          }
-        })
+            head: ["green"],
+            border: ["blue"],
+          },
+        };
 
-        table.push([
-          `${path.basename(dir)}`,
-          `${beforeSize} B`,
-          `${afterSize} B`
-        ]);
-        console.log(table.toString());
+        const outputTable = table ? table : new Table(tableConfig);
+        outputTable.push(...tableData);
+        console.log(outputTable.toString());
 
-        console.log(`Изображение ${fullPath} успешно сжато и сохранено в ${compressedPath}`);
+        console.log(
+          `Image ${fullPath} compressed & saved to ${compressedPath}`
+        );
       });
     }
   });
-}
+};
 
 const inputPath = process.argv[2];
-compressImages(inputPath)
+const table = new Table({
+  head: ["File", "Before", "After"],
+  style: {
+    head: ["green"],
+    border: ["blue"],
+  },
+});
+compressImages(inputPath, table);
