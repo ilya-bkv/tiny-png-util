ab10c96374e3eccbefa62c0d84cafe9fdf26e6b4
ilya-bkv <i.yablokov@ccmp.me>
2023-05-15 01:09:42 +0300

    working iteration

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tinify_1 = require("tinify");
+require('dotenv').config();
+var tinify = require("tinify");
 var fs = require("fs");
 var path = require("path");
 var Table = require("cli-table3");
-// Задаем API ключ Tinify API
-tinify_1.default.key = "3PGhpC1sZXGlBGpT3LEDzVCVn6iQyyAu";
-// Функция, которая обрабатывает все изображения в текущей директории и вызывает себя для всех поддиректорий
+tinify.key = process.env.API_KEY;
 var compressImages = function (dir, table) {
     fs.readdirSync(dir).forEach(function (file) {
         var fullPath = path.join(dir, file);
         if (fs.statSync(fullPath).isDirectory()) {
             compressImages(fullPath, table);
         }
-        else if (path.extname(fullPath).toLowerCase() === ".png" ||
-            path.extname(fullPath).toLowerCase() === ".jpg" ||
-            path.extname(fullPath).toLowerCase() === ".jpeg") {
-            // Если файл является изображением, сжимаем его и сохраняем сжатую версию в отдельный файл
+        else if ([".png", ".jpg", ".jpeg"].includes(path.extname(fullPath).toLowerCase())) {
             var imageDir = path.dirname(fullPath);
             var compressedDir = path.join(imageDir, "compressed");
             if (!fs.existsSync(compressedDir)) {
                 fs.mkdirSync(compressedDir);
             }
             var compressedPath_1 = path.join(compressedDir, path.basename(fullPath));
-            tinify_1.default.fromFile(fullPath).toFile(compressedPath_1, function (err) {
+            tinify.fromFile(fullPath).toFile(compressedPath_1, function (err) {
                 if (err) {
                     console.log(err);
                     return;
                 }
-                var beforeSize = fs.statSync(dir).size;
+                var beforeSize = fs.statSync(fullPath).size;
                 var afterSize = fs.statSync(compressedPath_1).size;
-                var table = new Table({
-                    head: ["File ", "Before", "After"],
+                var tableData = [
+                    [path.basename(fullPath), "".concat(beforeSize, " B"), "".concat(afterSize, " B")],
+                ];
+                var tableConfig = {
+                    head: ["File", "Before", "After"],
                     style: {
-                        head: ['green'],
-                        border: ['blue'],
-                    }
-                });
-                table.push([
-                    "".concat(path.basename(dir)),
-                    "".concat(beforeSize, " B"),
-                    "".concat(afterSize, " B")
-                ]);
-                console.log(table.toString());
-                console.log("\u0418\u0437\u043E\u0431\u0440\u0430\u0436\u0435\u043D\u0438\u0435 ".concat(fullPath, " \u0443\u0441\u043F\u0435\u0448\u043D\u043E \u0441\u0436\u0430\u0442\u043E \u0438 \u0441\u043E\u0445\u0440\u0430\u043D\u0435\u043D\u043E \u0432 ").concat(compressedPath_1));
+                        head: ["green"],
+                        border: ["blue"],
+                    },
+                };
+                var outputTable = table ? table : new Table(tableConfig);
+                outputTable.push.apply(outputTable, tableData);
+                console.log(outputTable.toString());
+                console.log("Image ".concat(fullPath, " compressed & saved to ").concat(compressedPath_1));
             });
         }
     });
 };
-// Получаем путь к файлу изображения или папке с изображениями
 var inputPath = process.argv[2];
-compressImages(inputPath);
+var table = new Table({
+    head: ["File", "Before", "After"],
+    style: {
+        head: ["green"],
+        border: ["blue"],
+    },
+});
+compressImages(inputPath, table);
